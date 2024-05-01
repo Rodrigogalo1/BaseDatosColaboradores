eec20425cb82bdc1ae492844930c49f56cfc46c6
Rodrigogalo1 <rodrigo.galo96@gmail.com>
2024-04-30 22:59:05 -0400

    Arreglos Significativos

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import React, { useState } from 'react';
 import { Form } from './components/Formulario';
 import { Listado } from './components/Listado';
 import { BaseColaboradores } from './components/BaseColaboradores';
+import Buscador from './components/Buscador';
 
 import './App.css';
 
 function App() {
   const [colaboradores, setColaboradores] = useState(BaseColaboradores);
-
+  const [busqueda, setBusqueda] = useState('');
+  const buscarColaboradores = (termino) => {
+    setBusqueda(termino);
+  };
+  const colaboradoresFiltrados = colaboradores.filter(colaborador => {
+    return colaborador.nombre.toLowerCase().includes(busqueda.toLowerCase());
+  });
   const agregarColaborador = (nuevoColaborador) => {
     setColaboradores([...colaboradores, nuevoColaborador]);
   };
@@ -15,7 +22,8 @@ function App() {
   return (
     <div>
       <h1>Listado de colaboradores</h1>
-      <Listado colaboradores={colaboradores} />
+      <Buscador onBuscar={buscarColaboradores} /> 
+      <Listado colaboradores={colaboradoresFiltrados} /> 
       <h1>Agregar colaborador</h1>
       <Form agregarColaborador={agregarColaborador} />
     </div>
---
+++ src/components/Buscador.jsx
@@ -0,0 +1,22 @@
+// Buscador.jsx
+import React from 'react';
+
+const Buscador = ({ onBuscar }) => {
+  const handleChange = (e) => {
+    const termino = e.target.value;
+    onBuscar(termino);
+  };
+
+  return (
+    <div className="mb-3">
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Buscar colaborador"
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
+
+export default Buscador;
